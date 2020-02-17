66ce552f7e55fb2377f99a26f79e86b5148fd5bf
Pam Deininger <pameladeininger@gmail.com>
2020-02-17 13:52:19 -0500

    corrections and formatting

--- app.js
+++ app.js
@@ -1,31 +1,94 @@
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+const Intern = "../lib/Intern";
+const Manager = "../lib/Manager";
+
 const inquirer = require("inquirer");
+const prompt = require("prompt");
+const fs = require("fs");
 
-const prompt = inquirer.createPromptModule();
+//const prompt = inquirer.createPromptModule();
 
-function getEngineer() {
-  inquirer
+function devTeam() {
+  return inquirer
     .prompt([
       {
         type: "input",
-        name: "name",
-        message: "Employee name:  "
+        name: "team",
+        message: "What is the name of the team?"
       },
       {
         type: "input",
-        name: "id",
-        message: "Employee ID:  "
+        name: "engineeerCount",
+        message: "How many Engineers are on the team?"
       },
       {
         type: "input",
-        name: "email",
-        message: "Employee email:  "
+        name: "internCount",
+        message: "How man Interns are on the team?"
       },
       {
         type: "input",
-        name: "github",
-        message: "GitHub user name:  "
+        name: "managerName",
+        message: "Who is the Manager"
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Provide the Managers Id?"
+      },
+      {
+        type: "input",
+        name: "managersEmail",
+        message: "Provide the Manager's email address?"
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Provide the Manager's contact number?"
+      },
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the name of the Engineer?"
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "Provide the Engineer's Id"
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "Provide the Engineer's email address"
+      },
+      {
+        type: "input",
+        name: "Engineer's GitHub",
+        message: "Provide the Engineer's GitHub"
+      },
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the name of the Intern?"
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "Provide the Intern's Id"
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "Provide the Intern's email address"
+      },
+      {
+        type: "input",
+        name: "internSchol",
+        message: "Where does the Intern attend school?"
       }
     ])
+
     .then(function(answers) {
       newemployee = new Engineer(
         answers.name,
@@ -35,33 +98,8 @@ function getEngineer() {
       );
       team.push(newEmployee);
       repeatQuery();
-    });
-}
+    })
 
-function getIntern() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Employee name:  "
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "Employee ID"
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Employee email:  "
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "GitHub user name:  "
-      }
-    ])
     .then(function(answers) {
       newemployee = new Intern(
         answers.name,
@@ -71,38 +109,13 @@ function getIntern() {
       );
       team.push(newEmployee);
       repeatQuery();
-    });
-}
+    })
 
-function getManager() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Employee name"
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "Employee ID"
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Employee email:  "
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "GitHub user name:  "
-      }
-    ])
     .then(function(answers) {
       newemployee -
-        new getManager(answers.name, answer.id, "Manager", answers.github
-        );
+        new getManager(answers.name, answer.id, "Manager", answers.github);
       team.push(newEmployee);
       repeatQuery();
     });
 }
+buildEmployee();
