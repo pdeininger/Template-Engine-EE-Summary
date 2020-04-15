b30a4ddca84909645c1098f9d8a5eda963641868
Pam Deininger <pameladeininger@gmail.com>
2020-04-15 19:38:46 -0400

    js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const inquirer = require("inquirer");
+const jest = require("jest");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
-const fs = require("fs");
 const path = require("path");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
 
 const render = require("./lib/htmlRenderer");
 
