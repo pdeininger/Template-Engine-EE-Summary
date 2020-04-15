1245322f9685203b40e05ad210285e4b6a387f6a
Pam Deininger <pameladeininger@gmail.com>
2020-04-15 18:56:04 -0400

    formatting

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const fs = require("fs");
+const path = require("path");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
 
 const employeeArray = [];
 let teamName = "";
